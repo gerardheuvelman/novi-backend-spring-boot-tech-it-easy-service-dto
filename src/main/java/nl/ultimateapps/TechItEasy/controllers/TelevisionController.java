fa3b98b80261cc87e05048d06f741a3e544bb7f3
gerardheuvelman <gerardheuvelman@gmail.com>
2022-11-14 10:23:52 +0100

    first commit

---
+++ src/main/java/nl/ultimateapps/TechItEasy/controllers/TelevisionController.java
@@ -0,0 +1,13 @@
+package nl.ultimateapps.TechItEasy.controllers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TelevisionController {
+
+    @GetMapping("/televisions")
+    public ResponseEntity<Object>
+}
