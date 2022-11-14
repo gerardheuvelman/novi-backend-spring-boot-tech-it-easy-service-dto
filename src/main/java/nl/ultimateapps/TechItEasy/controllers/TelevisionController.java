fc281e1046f0123063ba36f51f65ee3cf4e01158
gerardheuvelman <gerardheuvelman@gmail.com>
2022-11-14 10:40:26 +0100

    first commit

--- src/main/java/nl/ultimateapps/TechItEasy/controllers/TelevisionController.java
+++ src/main/java/nl/ultimateapps/TechItEasy/controllers/TelevisionController.java
@@ -1,13 +1,31 @@
 package nl.ultimateapps.TechItEasy.controllers;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TelevisionController {
 
     @GetMapping("/televisions")
-    public ResponseEntity<Object>
+    public ResponseEntity<String> getTelevisions () {
+//        return new ResponseEntity<>(HttpStatus.OK); kan ook
+        return ResponseEntity.ok("My televisions");
+    }
+
+    @GetMapping("/televisions/{id}")
+    public ResponseEntity<String> getTelevisions (@PathVariable int id) {
+//        return new ResponseEntity<>(HttpStatus.OK); kan ook
+        return ResponseEntity.ok("television " + id);
+    }
+
+
+
 }
+
+
+
+
